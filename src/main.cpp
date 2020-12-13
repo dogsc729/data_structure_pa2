608e62c815a7871fd18c4de6cd187385c2f399b2
dogsc729 <b07611033@ntu.edu.tw>
2020-12-13 19:39:45 +0800

    done?!

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,13 @@
 #include <iomanip>
 
 using namespace std;
+struct saltpassword
+{
+    string password;
+    string salt;
+    string hash_value;
+};
+vector<saltpassword> dictionary;
 long Hashfunction(long left, long right)
 {
     long hashvalue;
@@ -19,20 +26,68 @@ long Hashfunction(long left, long right)
 }
 int main()
 {
+    cout << "please enter the name of the passwordfile!" << endl;
+    string input;
+    cin >> input;
     fstream fin("./inputs/password.txt"), fout;
     fout.open("./outputs/dictionary.txt", ios::out);
     string password;
+    //read password.txt and output dictionary.txt
     vector<string> passwordset;
-    /*while (fin >> password)
+    vector<string> salt;
+    vector<string> hash_value;
+    while (fin >> password)
     {
-        passwordset.push_back(password);
-        fout << password << endl;
-    }*/
-    cout << "please enter a password!" << endl;
-    string input;
-    string numeinput;
-    cin >> input;
-    //transform the password to numeric value
+        string numepassword;
+        //transform the password to numeric value
+        for (int i = 0; i < password.size(); i++)
+        {
+            numepassword = numepassword + to_string(int(password[i]));
+        }
+        for (int i = 0; i < 1000; i++)
+        {
+            passwordset.push_back(password);
+            string temp;
+            if (i < 10)
+            {
+                temp = "00" + to_string(i) + numepassword;
+                salt.push_back(temp);
+            }
+            else if (i < 100)
+            {
+                temp = "0" + to_string(i) + numepassword;
+                salt.push_back(temp);
+            }
+            else
+            {
+                temp = to_string(i) + numepassword;
+                salt.push_back(temp);
+            }
+            long left = stol(temp.substr(0, 8));
+            long right = stol(temp.substr(8));
+            long hashvalue = Hashfunction(left, right);
+            hash_value.push_back(to_string(hashvalue));
+            fout << password << " "
+                 << setw(3) << setfill('0') << i
+                 << " " << hashvalue << endl;
+        }
+    }
+    cout << "please enter a hash value" << endl;
+    string target_hashvalue;
+    cin >> target_hashvalue;
+    for (int i = 0; i < hash_value.size(); i++)
+    {
+        if (target_hashvalue == hash_value[i])
+        {
+            cout << "the password is recovered!" << endl;
+            cout << "the password is " << passwordset[i] << endl;
+            cout << "the number of entries has been searched:" << i << endl;
+            return 0;
+        }
+    }
+    cout << "the password is not found!" << endl;
+    cout << "the number of entries has been searched:100000" << endl;
+    /*//transform the password to numeric value
     for (int i = 0; i < input.size(); i++)
     {
         numeinput = numeinput + to_string(int(input[i]));
@@ -52,11 +107,11 @@ int main()
         {
             temp = to_string(i) + numeinput;
         }
-        long left = stol(temp.substr(0,8));
+        long left = stol(temp.substr(0, 8));
         long right = stol(temp.substr(8));
         fout << input << " "
              << setw(3) << setfill('0') << i
-             << " " << Hashfunction(left,right) << endl;
-    }
+             << " " << Hashfunction(left, right) << endl;
+    }*/
     return 0;
 }
